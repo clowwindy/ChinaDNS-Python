ca57f46a0cd63fca1068d0c85a8379876b90f09f
alvie <alvie.zhang@gmail.com>
2015-06-27 08:17:53 +0800

    compatible to newer shadowsocks version

--- chinadns/dnsrelay.py
+++ chinadns/dnsrelay.py
@@ -35,7 +35,7 @@ if not (info[0] == 2 and info[1] >= 7):
     sys.exit(1)
 
 import argparse
-from shadowsocks import eventloop, asyncdns, lru_cache
+from shadowsocks import eventloop, asyncdns, lru_cache, common
 
 
 BUF_SIZE = 16384
@@ -92,7 +92,7 @@ class DNSRelay(object):
                     parts = line.split()
                     if len(parts) >= 2:
                         ip = parts[0]
-                        if asyncdns.is_ip(ip):
+                        if common.is_ip(ip):
                             for i in xrange(1, len(parts)):
                                 hostname = parts[i]
                                 if hostname:
